7e20a0745c225116fb7ad6c5edf05ebe8cd60a97
Sara Todorovikj <sara.todorovik@gmail.com>
2025-06-04 15:44:24 +0200

    add keyword search in abstract

--- src/recommender.py
+++ src/recommender.py
@@ -7,7 +7,9 @@ class Recommender:
         self.model = model
 
     def query(self, text, top_n=5):
-        match = self.df[self.df['title'].str.contains(text, case=False, na=False)] or self.df[self.df['abstract'].str.contains(text, case=False, na=False)]
+        match = self.df[self.df['title'].str.contains(text, case=False, na=False)] 
+        if match.empty:
+            self.df[self.df['abstract'].str.contains(text, case=False, na=False)]
         if not match.empty:
             idx = match.index[0]    
             query_embedding = self.embeddings[idx].reshape(1, -1)
@@ -19,4 +21,16 @@ class Recommender:
         else:
             raise ValueError(f"No papers found with title containing '{text}'.")
         
-   # def get_by_category(self, category, top_n=5):
+    def get_by_category(self, category, top_n=10):
+        if category not in self.df['main_category'].unique():
+            raise ValueError(f"Category '{category}' not found in the dataset.")
+        
+        category_df = self.df[self.df['main_category'] == category]
+        category_embeddings = self.embeddings[category_df.index]
+        
+        similarities = cosine_similarity(category_embeddings, self.embeddings).mean(axis=0)
+        top_indices = similarities.argsort()[-top_n:][::-1]
+        
+        results = self.df.iloc[top_indices].copy()
+        results['similarity'] = similarities[top_indices]
+        return results[['id', 'title', 'authors', 'abstract', 'categories', 'main_category', 'similarity']].to_dict(orient='records')
